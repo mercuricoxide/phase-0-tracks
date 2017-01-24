798a4ed1f7f40f0b25dad34cb138d4ae3f6542d0
Shea Thomas <arehanduro@gmail.com>
2017-01-24 13:32:44 -0500

    add hamster data program to hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,11 @@
+puts "Enter the name of the hamster"
+hname = gets.chomp
+puts "Enter the volume level of the hamster, between 1 and 10"
+hvol = gets.chomp.to_i
+puts "Enter the fur color of the hamster"
+hcol = gets.chomp
+puts "Is the hamster a good candidate for adoption? (y/n)"
+adopt = gets.chomp
+puts "What is the estimated age of the hamster? Please enter a number only."
+age = gets.chomp
+print "#{age}"
