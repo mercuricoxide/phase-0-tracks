0d1e8fa33839cc1f545a6062aa6e9d868e909c13
Shea Thomas <arehanduro@gmail.com>
2017-01-30 05:47:47 -0500

    finish releases 4 and 5

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -17,30 +17,46 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you? (
 garlic = gets.chomp
 puts "Would you like to enroll in the company health insurance? (y/n)"
 enroll = gets.chomp
+puts "Please list your allergies."
+$allergy = gets.chomp
 
 #determine if vampire
 
-if name == "Drake Cula" || name == "Tu Fang"
+loop do 
+  puts "any other allergies? If not, please enter 'done'"
+  @allergy = gets.chomp
+  break if @allergy == "done"
+  break if @allergy == "sunshine"
+end
+
+if $allergy == "sunshine" || @allergy == "sunshine"
+  puts "Probably a vampire"
+elsif name == "Drake Cula" || name == "Tu Fang"
   puts "Definitely a vampire"
 elsif age != 2017-dob && (garlic == "n" && enroll == "n")
   puts "Almost certainly a vampire"
-elsif age == 2017-dob && (garlic == "y" || enroll == "y")
-  puts "Probably not a vampire"
 elsif age != 2017-dob && (garlic == "n" || enroll == "n")
   puts "Probably a vampire"
+elsif age == 2017-dob && (garlic == "y" || enroll == "y")
+  puts "Probably not a vampire"
 else puts "Results inconclusive"
 end
 
 end
 
-#end program after every user has been processed
+#run program until every user has been processed
 
 while counter < num do determine_vampire
 counter += 1
 end
 
 #run program
+
 puts determine_vampire
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+
 
 
 
