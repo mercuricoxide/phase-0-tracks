684fd67ccb462838107dc240326d949a871bb6a9
Shea Thomas <arehanduro@gmail.com>
2017-02-08 13:56:16 -0500

    Complete 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Canis Lupus
+# 
+# CHARACTERISTICS ----------------
+# Self-grooming: No
+# Friendly: Yes
+# Leg Count: 4
+# Amount of Shedding: Varies
+# Fur Length: Varies
+# 
+# BEHAVIOR -----------------------
+# Sit
+# Beg
+# Roll Over
