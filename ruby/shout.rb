eccae7bd7594e51ce9ed34ea152c598b97fa75a5
Shea Thomas <arehanduro@gmail.com>
2017-02-13 15:37:22 -0500

    include code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,1 +1,33 @@
-#paste in code from practice file
+# module Shout
+
+#     def self.yell_angrily(words)
+#       words + "!!!" + " :("
+#     end
+
+#     def self.yellling_happily(words)
+#       words + "!" + " :D"
+#     end
+
+# end
+
+# p Shout.yell_angrily("shit")
+
+module Shout
+  def yell(words, mood)
+    puts words + "!!" + mood
+  end
+end
+
+class Happy
+  include Shout
+end
+
+class Angry
+  include Shout
+end
+
+happy = Happy.new
+happy.yell("yay", " :D")
+
+angry = Angry.new
+angry.yell("nooooo", " :(")
