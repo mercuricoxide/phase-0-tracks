76fbe98c41814a86e33b8efcf4f8d9f7462f9f65
Shea Thomas <arehanduro@gmail.com>
2017-02-13 03:04:32 -0500

    add new santa instances based on gender, ethnicity, sleigh, and weight

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,9 +5,29 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie)
-    puts "That was a good #{cookie}!"
+    puts "That was a good #{cookie} cookie!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity, sleigh, weight)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @sleigh = sleigh
+    @weight = weight
+  end
+
+  @reindeer_ranking = ["Rudolf", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  @age = 0
+
 end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_sleighs = ["holly", "birch", "yew", "mahogany", "ebony", "oak", "cherry"]
+example_weights_pounds = [250, 180, 200, 140, 400, 1500, 10]
+example_genders.length.times do |i|
+santas << Santa.new(example_genders[i], example_ethnicities[i], example_sleighs[i], example_weights_pounds[i])
+end
+
+p santas[2]
