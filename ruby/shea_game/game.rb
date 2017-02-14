e90e5f4b89cb4583aa5613f807f87475b27bd069
Shea Thomas <arehanduro@gmail.com>
2017-02-14 00:57:37 -0500

    add in code

--- ruby/shea_game/game.rb
+++ ruby/shea_game/game.rb
@@ -1,1 +1,48 @@
-#paste in code from practice file
+class WordGuessingGame
+  attr_reader :correct_guess
+  attr_accessor :user_guess
+
+  def initialize 
+    @correct_guess = "poop"
+    @guess_count = 0
+    @user_guess = ["morning", "poop", "zebra", "corn", "feet", "sky"]
+  end
+
+#Don't know how to make this work properly.
+
+  def feedback
+    display_string = ""
+      @correct_guess.chars.each do |char|
+        if @user_guess.sample.chars.include?(char)
+          display_string << char
+        else 
+         display_string << "_"
+        end
+      end
+    display_string
+  end
+
+  def check_word
+    @user_guess.sample
+      while @guess_count < @correct_guess.length && @user_guess.sample != @correct_guess
+        puts "WRONGGGGG! Try again!"
+        puts feedback
+        @guess_count += 1
+      end
+    puts "You won! You now get the actual word: #{@correct_guess}!!!"
+  end   
+
+end
+
+#user interface
+
+puts "Welcome to the Word Guessing Game!"
+game = WordGuessingGame.new
+
+puts "Try to guess the word!"
+
+puts game.check_word
+
+
+
+
