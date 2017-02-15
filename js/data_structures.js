e557f1306ea9b4809ba2170360d97c07568aba02
Shea Thomas <arehanduro@gmail.com>
2017-02-15 16:44:25 -0500

    add array of colors and horses

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+// array of colors
+var colors = ["red", "blue", "green", "yellow"];
+
+//array of horses
+var horses = ["charlie", "ponyta", "rapidash", "mudbray"];
+
+//add color to the colors array
+colors.push("orange");
+
+//add horse to the horses array
+horses.push("mudsdale");
+
+console.log(colors);
+
+console.log(horses);
