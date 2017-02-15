776ab7520a469634fe4c666bfe0dd070a0efd780
Shea Thomas <arehanduro@gmail.com>
2017-02-15 17:09:27 -0500

    create object of horses and their colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,15 +1,28 @@
 // array of colors
 var colors = ["red", "blue", "green", "yellow"];
 
-//array of horses
-var horses = ["charlie", "ponyta", "rapidash", "mudbray"];
+//array of horse names
+var names = ["charlie", "ponyta", "rapidash", "mudbray"];
 
 //add color to the colors array
 colors.push("orange");
 
 //add horse to the horses array
-horses.push("mudsdale");
+names.push("mudsdale");
 
-console.log(colors);
+//combine the two arrays of color and horses into one data structure
+//create an empty object and pass values inside
+
+var horses = {}
+
+//add key-value pairs by iterating through the two arrays
+
+//horses["charlie"] = "red"; (test case)
+
+//console.log(horses)
+
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
 
 console.log(horses);
