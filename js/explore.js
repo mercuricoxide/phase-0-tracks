a4dd07ba6d71672899b7b8fdde29aa688823c2a5
Shea Thomas <arehanduro@gmail.com>
2017-02-21 00:26:27 -0500

    add in code

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,17 @@
-//paste in code from practice
+//create a function that reverses a string
+//use the split method to turn the string into an array
+//use the reverse method to reverse the array
+//use the join method to turn the array back into a string
+
+function reverse(word) {
+  var makeArray = word.split("")
+  var reverseArray = makeArray.reverse();
+  var makeString = reverseArray.join("");
+  return makeString;
+}
+
+reverse_word = reverse("ditto")
+
+if (1 == 1) {
+  console.log(reverse_word)
+}
