34a51e7d9b5a7f1bd5ca8da304f559c6b3f4926e
Shea Thomas <arehanduro@gmail.com>
2017-03-03 13:54:36 -0500

    add kitty class and iterate over 50 instances of it

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#Puppy Class
+
 class Puppy
 
   def fetch(toy)
@@ -25,8 +27,32 @@ class Puppy
     puts "The dog will #{action}"
   end
 
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
 
 end
+
+#Kitty Class
+
+class Kitty
+
+  def initialize
+    puts "Welcome the new kitten to the family"
+  end
+
+  def sleep(hours)
+    puts "the kitten slept for #{hours} hours"
+  end
+
+  def purr(type_of_purr)
+    puts "the kitten is purring #{type_of_purr}"
+  end
+end
+
+
+#Driver code for the Puppy class
 
 labrador = Puppy.new
 
@@ -39,3 +65,30 @@ p labrador.roll_over
 p labrador.dog_years(3)
 
 p labrador.commands("sit")
+
+#Driver code for the Kitty class
+
+fluffy = Kitty.new
+
+p fluffy.sleep(4)
+
+p fluffy.purr("softly")
+
+#Array to hold all the cats
+
+Cat_family = []
+
+#filling the array with 50 new cats
+
+50.times do 
+  kitty = Kitty.new
+  Cat_family << kitty 
+end
+p Cat_family
+
+#having each of the cats sleep and purr
+
+Cat_family.each do |cat| 
+  cat.sleep(2)
+  cat.purr("normal")
+end
