dd87bdb5a0afe62d2a90e98ecbe5c4632285c2d3
Shea Thomas <arehanduro@gmail.com>
2017-03-03 13:24:06 -0500

    create puppy class, with fetch, speak, roll over, dog years, and commands methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,18 +1,3 @@
-# SPECIES ------------------------
-# Canis Lupus
-# 
-# CHARACTERISTICS ----------------
-# Self-grooming: No
-# Friendly: Yes
-# Leg Count: 4
-# Amount of Shedding: Varies
-# Fur Length: Varies
-# 
-# BEHAVIOR -----------------------
-# Sit
-# Beg
-# Roll Over
-
 class Puppy
 
   def fetch(toy)
@@ -21,83 +6,36 @@ class Puppy
   end
 
   def speak(num)
-    num.times do |i|
-      i = "woof"
-      p i
+    num.times do |talk|
+      talk = "woof"
+      puts talk
     end
   end
 
   def roll_over
-    return "rolls over"
+    puts "rolls over"
   end
 
   def dog_years(h_years)
     d_years = 7 * h_years
+    puts d_years
   end
 
-  def dog_commands
-    puts "Give a dog command:"
-    command = gets.chomp
-    if command == "sit"
-      puts "The dachshund puppy is sitting"
-    elsif command == "beg"
-      puts "The dachshund puppy is begging"
-    else puts "The dachshund puppy did not understand, try a basic command like 'sit' or 'beg'."
-    end
+  def commands(action)
+    puts "The dog will #{action}"
   end
 
-  def initialize
-    puts "Initializing new puppy instance ..."
-  end
 
 end
 
-dachshund = Puppy.new
+labrador = Puppy.new
 
-p dachshund.fetch("ball")
+p labrador.fetch("ball")
 
-p dachshund.speak(5)
+p labrador.speak(5)
 
-p dachshund.roll_over
+p labrador.roll_over
 
-p dachshund.dog_years(7)
+p labrador.dog_years(3)
 
-puts dachshund.dog_commands
-
-class Dog
-
-  def clean(body_part)
-    puts "The dog is cleaning his #{body_part}" 
-  end
-
-  def poop
-    puts "The dog is pooping"
-  end
-
-  def initialize
-    puts "Initializing new Dog instance ..."
-  end
-    
-  end
-
-array = []
-
-50.times do 
-  dogs = Dog.new
-  array << dogs
-end
-
-array.each do |i|
-  i.poop
-  i.clean("leg")
-end
-
-
-
-
-
-
-
-
-
-
+p labrador.commands("sit")
