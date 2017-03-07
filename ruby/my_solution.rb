f8c0535b00e21ca7d6624aae0477f713f4bab02a
Shea Thomas <arehanduro@gmail.com>
2017-03-06 19:07:59 -0500

    add in code

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -1,72 +1,61 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: Ian Driscoll ].
+# I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-# Require_relative is used to call data from separate files, allowing access to data from the same directory. Require is used to for ruby modules and gems.
+#
 #
 require_relative 'state_data'
 
 class VirusPredictor
-#responsible for declaring @state, @population, and @population_density variables and their values.
+
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
-    @i = 0
   end
-#responsible for declaring predicted_deaths and speed_of_spread methods and their arguments.
-  
+
   def virus_effects
-    predicted_deaths
-    speed_of_spread
+    predicted_deaths(@population_density, @population, @state)
+    speed_of_spread(@population_density, @state)
   end
 
   private
-#responsible for taking population_density, population, and state and calling them to produce a value for number_of_deaths for each state.
-  def predicted_deaths
+
+  def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
- 
- #refactored by creating instance variable for i and removing the equation for number_of_deaths from each number
-
     if @population_density >= 200
-     @i = 0.4
+      number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
-     @i = 0.3
+      number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
-     @i = 0.2
+      number_of_deaths = (@population * 0.2).floor
     elsif @population_density >= 50
-     @i = 0.1
+      number_of_deaths = (@population * 0.1).floor
     else
-     @i = 0.05
+      number_of_deaths = (@population * 0.05).floor
     end
-
-
-number_of_deaths = (@population * @i).floor
-    
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-#responsible for taking population_density and state and providing a value for speed in months that the outbreak will spread for each state.
-  def speed_of_spread #in months
+
+  def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-
-
-#refactored by removing speed = 0 and removing the + from the +=
+    speed = 0.0
 
     if @population_density >= 200
-      speed = 0.5
+      speed += 0.5
     elsif @population_density >= 150
-      speed = 1
+      speed += 1
     elsif @population_density >= 100
-      speed = 1.5
+      speed += 1.5
     elsif @population_density >= 50
-      speed = 2
+      speed += 2
     else
-      speed = 2.5
+      speed += 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -93,21 +82,6 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-STATE_DATA.each do |state_name, value|
- state_name = VirusPredictor.new("#{state_name}", value[:population_density], value[:population])
- state_name.virus_effects
-end 
 
 #=======================================================================
 # Reflection Section
-
-#1.The overall hash (the state name) uses hash rockets, while the population and density data are nested elements and use symbols.
-#2.Require_relative links data from another file to the coding file, allowing access to that data. Require is used for modules and gems.
-#3.You can iterate through a hash using the .each method.
-#4.Since the methods did not require input, we were able to remove the variables (arguments) from the methods in virus_effects.
-#5.This challenge helped solidify iteration, refactoring, and declaring instance variables.
-
-
-
-
-
