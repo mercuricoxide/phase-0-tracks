1f754d6b34fb020a2e69bf6ddb15a87065e9b776
Shea Thomas <arehanduro@gmail.com>
2017-03-27 08:18:38 -0400

    clean up

--- js/algos.js
+++ js/algos.js
@@ -1,1 +1,16 @@
-//paste in code from practice
+function largest(array) {
+  
+  var largest_phrase;
+
+  var largest_length = 0;
+
+  for (var i=0; i < array.length; i++){
+    if (array[i].length > largest_length){
+      largest_length = array[i].length;
+      largest_phrase = array[i];
+    }
+  }
+  return largest_phrase
+}
+
+console.log(largest(["hello", "good morning", "goodbye"]));
